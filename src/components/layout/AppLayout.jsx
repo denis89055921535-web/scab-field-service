f6e3dd083491193216f9cef23f30ae4dd3c1251c
zhahfu <zhahfu@xevorin.com>
2026-05-07 11:49:53 +0000

    File changes

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -4,8 +4,8 @@ import BottomNav from './BottomNav';
 export default function AppLayout() {
   return (
     <div className="min-h-screen bg-background font-inter">
-      <header className="text-white px-4 py-2 flex items-center justify-center bg-[#133371]">
-        <img src="https://media.base44.com/images/public/69fc64a1b3cf2f52f788409b/f04edf800_ChatGPTImage7202614_43_48.png" alt="SCAB" className="h-10 object-contain" />
+      <header className="text-white px-4 py-2 flex items-center justify-start bg-[#133371]">
+        <img src="https://media.base44.com/images/public/69fc64a1b3cf2f52f788409b/e809c3b3c_ChatGPTImage7202614_48_13.png" alt="SCAB" className="h-10 object-contain" />
       </header>
       <main className="pb-20 max-w-lg mx-auto">
         <Outlet />
