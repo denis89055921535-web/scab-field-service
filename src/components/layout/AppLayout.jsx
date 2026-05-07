2ccd942bc173108aa70e45e7faf8ef7c563a6635
zhahfu <zhahfu@xevorin.com>
2026-05-07 11:45:24 +0000

    File changes

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -5,7 +5,7 @@ export default function AppLayout() {
   return (
     <div className="min-h-screen bg-background font-inter">
       <header className="text-white px-4 py-2 flex items-center justify-center bg-[#133371]">
-        <img src="https://media.base44.com/images/public/69fc64a1b3cf2f52f788409b/3475448f0_62487d37-7f90-4b40-b584-5a3445e0d554.png" alt="SCAB" className="h-10 object-contain" />
+        <img src="https://media.base44.com/images/public/69fc64a1b3cf2f52f788409b/f04edf800_ChatGPTImage7202614_43_48.png" alt="SCAB" className="h-10 object-contain" />
       </header>
       <main className="pb-20 max-w-lg mx-auto">
         <Outlet />
